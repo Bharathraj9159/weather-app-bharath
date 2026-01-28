58b4935cebb99d541ea9a9b285b51c4850cdcb85
Bharathraj9159 <bharathrajgr8760@gmail.com>
2026-01-28 12:21:47 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -115,6 +115,7 @@ const App = () => {
   };
 
   // ✅ Automatically get location on first load
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     getLocationWeather();
   }, []);
